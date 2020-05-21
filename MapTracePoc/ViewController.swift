a9f3e25de8af8a7fcf67fb6d7e91af5b57081cc1
Fang Chen <cfxyz104@gmail.com>
2020-05-21 15:34:07 +0800

    Initial Commit

---
+++ MapTracePoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapTracePoc
+//
+//  Created by Fang Chen on 5/21/20.
+//  Copyright © 2020 Fang Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
